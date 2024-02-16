98285de94d3425ac5c7af5f489acf2aca505da4c
OzodbekMahmaraimov <ozodebkmahmaraimov07@gmail.com>
2024-02-16 17:50:26 +0500

    react icon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Asadbek from './components/Asadbek/Asadbek';
+import Ozodbek from './components/Ozodbek/Ozodbek';
+import Asilbek from './components/Asilbek/Asilbek';
+import Javlon from './components/Javlon/Javlon';
+import Muhammad from './components/Muhammad/Muhammad';
+import Zuhriddin from './components/Zuhriddin/Zuhriddin';
 
 function App() {
   return (
     <>
-    
+    <Routes>
+      <Route path="/" Component={Ozodbek} />
+      <Route path="/asadbek" Component={Asadbek} />
+      <Route path="/asilbek" Component={Asilbek} />
+      <Route path="/javlon" Component={Javlon} />
+      <Route path="/muhammad" Component={Muhammad} />
+      <Route path="/zuhriddin" Component={Zuhriddin} />
+    </Routes>
     </>
   );
 }
