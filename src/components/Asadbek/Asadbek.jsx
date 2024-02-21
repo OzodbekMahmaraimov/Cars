9297f6b623488bd0f4621e4c499f5ea7fee83d7e
asabrek <klubasa462@gmail.com>
2024-02-21 14:18:02 +0500

    maniki Tayyor bulli

--- src/components/Asadbek/Asadbek.jsx
+++ src/components/Asadbek/Asadbek.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Card from './Card/Cards'
+import Cards2 from './card2/Card2'
 
 const Asadbek = () => {
   return (
    <>
    <Card/>
+   <Cards2/>
    </>
   )
 }
